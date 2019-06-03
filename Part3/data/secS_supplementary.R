2280c1e1de748af5139b764dd8b6acd5a6943aa9
Seongdong Kim <sdkim0718@naver.com>
2019-06-03 22:00:08 +0900

    Part 3, suppiementary

---
+++ Part3/data/secS_supplementary.R
@@ -0,0 +1,50 @@
+library(ggplot2)
+View(mpg)
+library(dplyr)
+mpg %>%
+  filter(manufacturer %in% c('audi','toyota')) %>%
+  group_by(manufacturer) %>%
+  summarise(고속도로연비평균 = mean(hwy))
+
+mpg %>%
+  filter(manufacturer %in% c('chevrolet','ford','honda')) %>%
+  group_by(manufacturer) %>%
+  summarise(city = mean(cty))
+
+mpg %>%
+  select(drv,cty,hwy) %>%
+  mutate((cty + hwy)/2)
+
+mpg %>%
+  mutate(avg_spd = (cty + hwy) / 2) %>%
+  group_by(class) %>%
+  summarise(class별평균속도 = mean(avg_spd)) %>%
+  arrange(desc(class별평균속도)) %>%
+  head(5)
+
+mpg %>%
+  mutate(avg_spd = (cty + hwy) / 2) %>%
+  select(manufacturer, class, cty, hwy, avg_spd) %>%
+  head(5)
+
+mtcars %>%
+  mutate(mpg_per_disp = (mpg / disp)) %>%
+  arrange(desc(mpg_per_disp)) %>%
+  head(5)
+
+mtcars %>%
+  group_by(cyl) %>%
+  summarise(avg_hp = mean(hp)) %>%
+  arrange(desc(avg_hp))
+
+mtcars %>%
+  group_by(cyl) %>%
+  summarise_each(list(mean), disp, hp)
+
+mtcars %>%
+  group_by(cyl) %>%
+  summarise_each(funs(mean, n()), disp, hp)
+
+iris %>%
+  group_by(Species) %>%
+  summarise_each(list(mean), Sepal.Length, Sepal.Width, Petal.Length, Petal.Width)
