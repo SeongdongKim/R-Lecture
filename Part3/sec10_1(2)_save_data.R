70a53cc006137464b978b321edaa4c7d49096ed7
Seongdong Kim <sdkim0718@naver.com>
2019-06-07 17:50:55 +0900

    그래픽기초

---
+++ Part3/sec10_1(2)_save_data.R
@@ -0,0 +1,9 @@
+sum <- 0
+for (i in seq(3, 100, 3)) {
+  sum <- sum + i
+}
+print(sum)
+
+for(i in c(1,3,5,7,9)) {
+  print(i)
+}
---
+++ Part4/저수준 작도 함수.R
@@ -0,0 +1,14 @@
+# 저수준 작도 함수
+var1 <- 1:5
+plot(var1)
+segments(2,2,3,3)
+arrows(5,5,4,4)
+text(2,4,"labels")
+text(3,2,"테스트",srt=45)
+
+plot(1:15)
+abline(h=8)
+rect(1,6,3,8)
+arrows(1,1,5,5)
+text(8,9,"TEXT")
+title("THIS IS TEST", "SUB")
